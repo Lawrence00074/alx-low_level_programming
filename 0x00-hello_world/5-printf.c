bfe333c546ffd73c05f171a9e7bf1fb623d7af41
Lawrence00074 <lawrence0074real@gmail.com>
2023-06-17 04:45:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
  * main - To print in C progarmming using the function printf
  * Return: 0 (Success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n";)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
