3e47007709b15dd68b6fce32a0a4a7ff0e29820e
Lawrence00074 <lawrence0074real@gmail.com>
2023-06-17 03:04:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function, without allowed to use anothe fumction
+ * Return: Always 0
+ */
+int main(viod)
+{
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
+}
