4efd34f7194b365e34b87447525db6469423547b
Lawrence00074 <lawrence0074real@gmail.com>
2023-06-17 18:22:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+char t;
+char p;
+t = 'a';
+p = 'A';
+while
+(t <= 'z') {
+putchar(t);
+t++;
+}
+while
+(p <= 'Z') {
+putchar(p);
+p++;
+}
+putchar('\n');
+return (0);
+}
