0c80b2f9992a824259fac25ddf58d6c0b4a350c0
Lawrence00074 <lawrence0074real@gmail.com>
2023-06-17 19:16:23 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int o = 97;
+while (o <= 122)
+{
+if
+(o == 101 || o = 113)
+{
+o++;
+continue;
+}
+putchar(o);
+o++;
+}
+putchar('\n');
+return (0);
+}
