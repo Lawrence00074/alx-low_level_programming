ae40f3e10f05302d1c381f58ccb2c371e6d88224
Lawrence00074 <lawrence0074real@gmail.com>
2023-06-17 20:37:25 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char t;
+int o;
+t = 'a';
+o = 0;
+while
+(o < 10) {
+putchar(o + '0');
+o++;
+}
+while
+(t <= 'f') {
+putchar(t);
+t++;
+}
+putchar('\n');
+return (0);
+}
