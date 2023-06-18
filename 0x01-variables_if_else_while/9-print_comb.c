b6c8620a94ec86f669550e54e04f9e01fa944c2d
Lawrence00074 <lawrence0074real@gmail.com>
2023-06-17 23:58:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int l;
+for (l = 0; l < 10; l++)
+{
+putchar(l + '0');
+if (l < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
