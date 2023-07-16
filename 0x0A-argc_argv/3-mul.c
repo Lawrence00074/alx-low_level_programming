ac441307e89c2f838e16f7f1b650b0ad11cf243b
Lawrence00074 <lawrence0074real@gmail.com>
2023-07-15 19:07:05 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiplies two numbers
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+int t, o;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+t = _atoi(argv[1]);
+o = _atoi(argv[2])
+printf("%d\n", t + o);
+return (0);
+}
