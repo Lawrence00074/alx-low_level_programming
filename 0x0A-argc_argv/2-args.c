dc08e20b1418573bac986be0937b7e5aa949f06d
Lawrence00074 <lawrence0074real@gmail.com>
2023-07-15 17:31:38 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,14 +4,14 @@
  * main - prints all arguments it receives
  * @argc: arguments count
  * @argv: arguments
- * Return: Always 0 (Success)
+ * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int t;
-for (t = 0, t < argc; t++)
+int i;
+for (i = 0; i < argc; i++)
 {
-printf("%s\n", argv[t]);
+printf("%s\n", argv[i]);
 }
 return (0);
 }
