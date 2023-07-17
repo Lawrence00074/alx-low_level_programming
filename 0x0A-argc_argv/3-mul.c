638a55d7f12523cca57b2d03a3fa182299ed4166
Lawrence00074 <lawrence0074real@gmail.com>
2023-07-17 01:59:53 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: arguments count
  * @argv: arguments
- * Return: 0
+ * Return: 0 (Success)
  */
 int main(int argc, char **argv)
 {
@@ -15,7 +15,7 @@ printf("Error\n");
 return (1);
 }
 t = atoi(argv[1]);
-o = atoi(argv[2])
-printf("%d\n", t + o);
+o = atoi(argv[2]);
+printf("%d\n", t * o);
 return (0);
 }
