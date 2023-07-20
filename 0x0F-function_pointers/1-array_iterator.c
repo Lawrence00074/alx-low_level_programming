3357a7cad445c6eb310b84dd0cfcc4193832526b
Lawrence00074 <lawrence0074real@gmail.com>
2023-07-20 12:19:09 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int t;
 if (array == NULL || action == NULL)
 return;
-for (t = 0; t < size, t++)
+for (t = 0; t < size; t++)
 {
 action(array[t]);
 }
