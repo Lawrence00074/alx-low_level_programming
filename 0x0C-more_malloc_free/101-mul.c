66389243bb70a418e6f6dbbb67ef2169f821adb3
Lawrence00074 <lawrence0074real@gmail.com>
2023-07-23 12:33:12 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+unsigned long mul;
+int t, o;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+for (t = 1; t < argc; t++)
+{
+for (o = 0; argv[o][o] != '\0'; o++)
+{
+if (argv[t][o] > 57 || argv[t][o] < 48)
+{
+printf("Error\n");
+exit(98);
+}
+}
+}
+mul = atol(argv[1]) * atol(argv[2]);
+printf("%lu\n", mul);
+return (0);
+}
