c1e0d7e3f62985b15cf2d1ff8e6728e02ba417f0
Lawrence00074 <lawrence0074real@gmail.com>
2023-09-28 14:04:00 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - clear bit to 0 at given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int max_size;
+max_size = (sizeof(max_size) * 8);
+if (index > max_size)
+return (-1);
+*n = (*n & ~(1 << index));
+return (1);
+}
